08d25a63fc35f2365bc8961c0b3e50fd094de012
gitpk-0 <kell108p@protonmail.com>
2022-06-09 10:02:15 -0400

    login module created

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+from issue_tracker.helpers import apology
+
 # from helpers import apology, login_required, lookup, usd
 
 # Configure application
@@ -37,6 +39,10 @@ def get_db_connection():
     return conn
 
 
+conn = get_db_connection
+db = conn.cursor()  # Database connection
+
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -47,7 +53,7 @@ def after_request(response):
 
 
 @app.route("/")
-# @login_requirred
+@login_requirred
 def index():
     """ Landing page """
     conn = get_db_connection()
@@ -58,3 +64,37 @@ def index():
     conn.close()
 
     return render_template('index.html', users=users)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """ Log user in """
+
+    # Forget previous user_id
+    session.clear()
+
+    # User reached route via POST
+    if request.method == "POST":
+        username = request.form.get("username")
+        pw = request.form.get("password")
+
+        # Ensure username was submitted
+        if not username:
+            return apology("no username entered", 403)
+
+        # Ensure password was submitted
+        elif not pw:
+            return apology("no password entered", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * from users WHERE username = ?", username)
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], pw):
+            return apology("invalid username or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
